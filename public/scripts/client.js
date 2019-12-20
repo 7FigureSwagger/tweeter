f4be1990ed0ba4171a83fe5198376d5193c7742f
7 <swistek.labs@gmail.com>
2019-12-20 23:32:14 +0000

    update

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,79 +5,95 @@
  */
 
 $(document).ready(function() {
-  $(".tweet").on("submit", function(event) {
-   
-    $(".tContainer").append($tweet);
-	});
+	const getTweet = search => {
+		let tweet = {};
+	};
 
-
-  const tweetData = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
+	$(".flexForm").on("submit", function(event) {
+    event.preventDefault();
+    let text = $('#mainText').val();
+		console.log("data", text);
+    
+    if (text.length > 140 || text === '') {
+      $('#error').slideToggle('fast').delay( 500 ).slideUp('fast');       
+    } else {
+			$.ajax({
+				url: "/tweets",
+				data: $(this).serialize(),
+				type: "POST",
+				dataType: "JSON"
+    })
+    .then(loadTweets());    
     }
-  ]
+  });
 
-const createTweetElement = function(tweet) {
-  let date = new Date(tweet.created_at).toDateString();
-  const $tweets = `
+ 
+
+  const escape =  function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
+
+  const createTweetElement = function(tweet) {
+    let date = new Date(tweet.created_at).toDateString();
+    let heart = 'class=ui-icon-heart'
+		const $tweets = `
     <article class='tweets'>
-      <header>
-        <img src="${tweet.user.avatars}">
-        ${tweet.user.name}
-      <a class='tweet-handle'>${tweet.user.handle}</a>
+      <header class='nTweetHeader'>
+        <div>
+          <img class="nTweetAvatar" src="${tweet.user.avatars}">
+          <span>${tweet.user.name}</span>
+        </div>
+        <a class='tweet-handle'>${tweet.user.handle}</a>
       </header>
-      <span class='tweet-body'>${tweet.content.text}</span>
+      <span class='tweet-body'>${escape(tweet.content.text)}</span>
       <footer>
         <div class="tweet-age">${date}</div>
-        <div class='tweet-icon'>
+        <div class="nTweetIcon" class='tweet-icon'>
           <i class="fa fa-flag"></i>
           <i class="fa fa-heart"></i>
-          <i class="fa fa-refresh"></i>
+          <i class="fa fa-thumbs-up"></i>
         </div>
       </footer>
     </article>
   `;
-  return $tweets;
-};
+		return $tweets;
+	};
 
+	const renderTweets = function(tweets) {
+		// loops through tweets
+		// calls createTweetElement for each tweet
+		// takes return value and appends it to the tweets container
+		for (let x of tweets) {
+			let $tweet = createTweetElement(x);
+			$(".tContainer").prepend($tweet);
+		}
+	};
 
-const renderTweets = function(tweets) {
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
-  for (let x of tweets) {
-    let $tweet = createTweetElement(x);
-    $('.tContainer').append($tweet);
-  }
-}
+	const loadTweets = function() {
+		$.ajax({
+			url: "/tweets",
+			type: "GET",
+			dataType: "JSON"
+		}).then(data => {
+      // console.log(data.reverse());
+      console.log('tweet', data);
+			renderTweets(data);
+		});
+	};
 
+	loadTweets();
 
+  $('#mainText').keyup(function(e){
+    if(e.which === 13)
+       $('#tweetButt').click();
+  });
 
-
-renderTweets(tweetData);
-
-
-// const $tweet = createTweetElement(tweetData);
-// Test / driver code (temporary)
-
-
+  $( "#nTweetTag" ).click(function() {
+    $( "#form" ).slideDown( "fast", function() {
+      $('#mainText').focus();
+    })
+  });
+  
 });
